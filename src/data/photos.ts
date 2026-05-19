5f7254a97de5887c8ca421efc20bb75c4f3534d3
zoe-the-happiness <zoethehappiness@gmail.com>
2026-05-19 17:21:56 +0530

    feat: Add more hero photos
    Increases the number of photos displayed in the hero section to 8, enhancing visual variety.

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,4 +1,7 @@
 export const allPhotos = [
+  "https://ik.imagekit.io/dgevdmg7em/AVD/q5ezakfiqrvvbgbumikm.webp",
+  "https://ik.imagekit.io/dgevdmg7em/AVD/ttgl9lzpzlokfotpx4kd.webp",
+  "https://ik.imagekit.io/dgevdmg7em/AVD/xhsxm4ger7r8pccw8imx.webp",
   "https://ik.imagekit.io/dgevdmg7em/AVD/20260519%20AVD%20Photos%20(8).webp?updatedAt=1779185033992",
   "https://ik.imagekit.io/dgevdmg7em/AVD/20260519%20AVD%20Photos%20(9).webp?updatedAt=1779185033877",
   "https://ik.imagekit.io/dgevdmg7em/AVD/20260519%20AVD%20Photos%20(13).webp?updatedAt=1779185032837",
@@ -15,4 +18,5 @@ export const allPhotos = [
   "https://ik.imagekit.io/dgevdmg7em/AVD/20260519%20AVD%20Photos%20(1).webp?updatedAt=1779185031856"
 ];
 
-export const heroPhotos = allPhotos.slice(0, 5);
+export const heroPhotos = allPhotos.slice(0, 8);
+
