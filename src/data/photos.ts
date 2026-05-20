7dc62ade70a74eb1e7f64f46c639b13304133f77
zoe-the-happiness <zoethehappiness@gmail.com>
2026-05-20 15:40:13 +0530

    feat(home): Adjust hero carousel interval and update photos
    The interval for the hero background carousel has been reduced to improve user engagement. Additionally, the `allPhotos` array has been updated to include a more diverse set of images and remove duplicates, ensuring a refreshed visual experience on the home page.

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,11 +1,10 @@
 export const allPhotos = [
   "https://ik.imagekit.io/avdwb/ttgl9lzpzlokfotpx4kd.webp?updatedAt=1779211452815",
-  "https://ik.imagekit.io/avdwb/xhsxm4ger7r8pccw8imx.webp?updatedAt=1779211452662",
+  "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(4).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(1).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(2).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(12).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(3).webp",
-  "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(4).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(7).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(14).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(5).webp",
@@ -14,6 +13,8 @@ export const allPhotos = [
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(8).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(9).webp",
   "https://ik.imagekit.io/avdwb/q5ezakfiqrvvbgbumikm.webp",
+  "https://ik.imagekit.io/avdwb/xhsxm4ger7r8pccw8imx.webp",
+  "https://ik.imagekit.io/avdwb/ttgl9lzpzlokfotpx4kd.webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(10).webp",
   "https://ik.imagekit.io/avdwb/20260519%20AVD%20Photos%20(6).webp"
 ];
